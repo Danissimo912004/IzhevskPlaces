6999cce29b9a85789d6cb160762aee1697ad4c7f
Danissimo912004 <157467778+Danissimo912004@users.noreply.github.com>
2025-05-27 21:11:57 +0400

    Lab4

--- app/src/main/java/com/example/izhevskplaces/MainActivity.kt
+++ app/src/main/java/com/example/izhevskplaces/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-
+import com.example.izhevskplaces.data.IzhevskPlacesApp
 import com.example.izhevskplaces.UI.IzhevskPlacesTheme
 import com.example.izhevskplaces.UI.PlacesViewModel
 
